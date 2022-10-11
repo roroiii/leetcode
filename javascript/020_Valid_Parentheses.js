d8a9f3244671e1b3e7a40da41470e243fd15ba42
Tina <rorobioao@gmail.com>
2022-10-11 17:30:27 +0800

    add 20 Valid Parentheses

---
+++ javascript/020_Valid_Parentheses.js
@@ -0,0 +1,53 @@
+/**
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ * Difficulty:Easy
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+*/
+
+var isValid = function (s) {
+  if (s.length % 2 === 1) return false;
+  const arr = [];
+
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
+      case "(":
+        arr.push(")");
+        break;
+      case "[":
+        arr.push("]");
+        break;
+
+      case "{":
+        arr.push("}");
+        break;
+
+      default:
+        if (arr.pop() !== s[i]) return false;
+    }
+  }
+  return !arr.length;
+};
+
+isValid("()");
+isValid("()[]{}");
+isValid("(]");
+isValid("{[]}");
